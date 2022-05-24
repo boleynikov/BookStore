93411c4f1f591e38b11c31aa1cf8a01a86a92a1d
Богдан Олейников <ol.bogdan13@gmail.com>
2022-05-24 12:19:36 +0300

    recreate new repo

---
+++ domain/Store/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class OrderItem
+    {
+        public int BookId { get; }
+        public int Count { get; }
+        public decimal Price { get; }
+        public OrderItem(int bookId, int count, decimal price)
+        {
+            if (count < 1)
+                throw new ArgumentOutOfRangeException("Count must be greater than zero");
+            BookId = bookId;
+            Count = count;
+            Price = price;
+        }
+    }
+}
---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "ISBN 13246-65348", "D. Knuth", "Art Of Programming", "Knuth began the project, originally conceived as a single book with twelve chapters, in 1962. The first three volumes of what was then expected to be a seven-volume set were published in 1968, 1969, and 1973. Work began in earnest on Volume 4 in 1973, but was suspended in 1977 for work on typesetting prompted by the second edition of Volume 2.", 1300m),
+            new Book(2, "ISBN 13246-65349", "M. Fowler", "Refactoring", "Refactoring is a controlled technique for improving the design of an existing code base. Its essence is applying a series of small behavior-preserving transformations, each of which \"too small to be worth doing\".", 650m),
+            new Book(3, "ISBN 13246-65350", "B. Kernighan, D. Ritchie", "C Programming Language", "The authors present the complete guide to ANSI standard C language programming. Written by the developers of C, this new version helps readers keep up with the finalized ANSI standard for C while showing how to take advantage of C's rich set of operators, economy of expression, improved control flow, and data structures. The 2/E has been completely rewritten with additional examples and problem sets to clarify the implementation of difficult language constructs.", 950m),
+         };
+
+        public Book[] GetAllByIds(IEnumerable<int> bookIds)
+        {
+            var fountBooks = from book in books
+                             join bookId in bookIds on book.Id equals bookId
+                             select book;
+            return fountBooks.ToArray();
+
+        }
+
+        public Book[] GetAllByIsbn(string isbn)
+        {
+            return books.Where(book => book.Isbn.Contains(isbn))
+                        .ToArray();
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string query)
+        {
+            return books.Where(book => book.Author.Contains(query)
+                                    || book.Title.Contains(query))
+                                      .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
+        }
+    }
+}
